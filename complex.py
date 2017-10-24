e47a4b4009f37abac356b1070f4392a7b1d332f9
Lil-Bech <31544941+Lil-Bech@users.noreply.github.com>
2017-10-24 09:06:06 -0700

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,7 @@
+def main():
+	print("Complex numbers in python")
+	print(type(1+1j))
+	A = 'A'
+	print(type(a))
+	
+main(), main()	
---
+++ yes.py
@@ -0,0 +1,35 @@
+
+from turtle import *
+
+speed(.01) 
+pencolor('#FFF')
+bgcolor('orange')
+
+x = 0
+up() 
+
+
+rt(46) 
+fd(91) 
+rt(136) 
+
+down() 
+while x < 600:
+                
+    fd(5)     
+    rt(11)
+    fd(5)
+    rt(11)
+    fd(5)
+    rt(11)
+    fd(5)
+    rt(11)
+    fd(5)
+    rt(11)
+    fd(5)
+    rt(11)
+
+    rt(11.1111) 
+    x = x+1 
+
+exitonclick() 
